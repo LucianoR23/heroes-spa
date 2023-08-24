39c403cc879e6f6d0683d8d843e90e96879f9d72
LucianoR23 <231097lr@gmail.com>
2023-08-24 18:08:52 -0300

    Mejorado mobile

--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -1,6 +1,7 @@
 import { useMemo } from "react"
 import { HeroCard } from "./"
 import { getHeroesByPublisher } from "../helpers/getHeroesByPublisher"
+import './heroList.css'
 
 
 export const HeroList = ({ publisher }) => {
@@ -8,7 +9,7 @@ export const HeroList = ({ publisher }) => {
     const heroes = useMemo ( () => getHeroesByPublisher( publisher ), [ publisher ] )
 
     return (
-        <div className="row rows-cols-1 row-cols-sm-3 g-4">
+        <div className="row rows-cols-1 row-cols-sm-3 g-1 hero-phone">
             {
                 heroes.map( (hero) => (
                     <HeroCard key={hero.id} {...hero} />
--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -26,8 +26,8 @@ export const Navbar = () => {
             >
                 Associations
             </Link>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
             </button>
 
             <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
